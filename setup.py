d5d54d17518b8c12532a1dbaf27c82c3345ad38a
Gordon Syme <gordonsyme@gmail.com>
2013-07-24 16:47:37 -0700

    Replicate setuptools/distribute bug

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+        name='distribute_setuptools',
+        version='1.0',
+        description='replicate stupid bug',
+        author='Gordon Syme',
+        author_email='gordonsyme@gmail.com',
+     )
