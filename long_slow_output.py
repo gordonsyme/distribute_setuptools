d23dab97697910b69221bcbc69a464767e2bc10e
Gordon Syme <gordonsyme@gmail.com>
2013-10-02 18:21:23 +0100

    Output doesn't matter, the command being sent to the container is what matters

--- long_slow_output.py
+++ long_slow_output.py
@@ -2,10 +2,6 @@ import sys
 import time
 from datetime import datetime, timedelta
 
-def output_madly_long_string(character_count):
-    for i in xrange(character_count):
-        print "*",
-
 def slowly_slowly_outputee_monkey(character, duration=timedelta(minutes=1), sleep_time=timedelta(seconds=1)):
     end = datetime.utcnow() + duration
     while datetime.utcnow() < end:
@@ -14,5 +10,5 @@ def slowly_slowly_outputee_monkey(character, duration=timedelta(minutes=1), slee
         time.sleep(sleep_time.total_seconds())
 
 if __name__ == '__main__':
-    output_madly_long_string(25000)
-    slowly_slowly_outputee_monkey(".", duration=timedelta(minutes=30), sleep_time=timedelta(milliseconds=100))
+    print sys.argv
+    slowly_slowly_outputee_monkey(".", duration=timedelta(minutes=1), sleep_time=timedelta(milliseconds=100))
